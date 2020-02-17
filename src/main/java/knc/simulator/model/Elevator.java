2bd2b25ea7aa1cf8fc1022c336481905882da83f
Robin Larsson <robin_rtl@hotmail.com>
2020-02-17 20:22:29 +0100

    Created a model of the elevator

--- src/main/java/knc/simulator/model/Elevator.java
+++ src/main/java/knc/simulator/model/Elevator.java
@@ -1,4 +1,84 @@
 package knc.simulator.model;
 
 public class Elevator {
+    private ElevatorAction currentAction = ElevatorAction.IDLE;
+    private int maxHoldTime = 100;
+    private int currentHoldTime = 0;
+    private double speed = 0.01;
+    private double currentPosition;
+    private int targetStorey;
+
+    public int getTargetStorey() {
+        return targetStorey;
+    }
+
+    public void setTargetStorey(int targetStorey) {
+        this.targetStorey = targetStorey;
+
+        if(targetStorey > currentPosition) {
+            currentAction = ElevatorAction.GOING_UP;
+        } else if(targetStorey < currentPosition) {
+            currentAction = ElevatorAction.GOING_DOWN;
+        } else {
+            currentAction = ElevatorAction.IDLE;
+        }
+    }
+
+    public ElevatorAction getCurrentAction() {
+        return currentAction;
+    }
+
+    public double getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void update() {
+        switch(currentAction) {
+            case IDLE:
+                break;
+            case HOLD:
+                hold();
+                break;
+            case GOING_UP:
+                moveUpwards();
+                break;
+            case GOING_DOWN:
+                moveDownwards();
+                break;
+        }
+    }
+
+    private void hold() {
+        if(currentHoldTime++ >= maxHoldTime) {
+            exitHold();
+        }
+    }
+
+    private void moveUpwards() {
+        currentPosition += speed;
+        if(currentPosition >= targetStorey) {
+            destinationReached();
+        }
+    }
+
+    private void moveDownwards() {
+        currentPosition -= speed;
+        if(currentPosition <= targetStorey) {
+            destinationReached();
+        }
+    }
+
+    private void destinationReached() {
+        currentPosition = targetStorey;
+        startHold();
+    }
+
+    private void startHold() {
+        currentHoldTime = 0;
+        currentAction = ElevatorAction.HOLD;
+    }
+
+    private void exitHold() {
+        currentAction = ElevatorAction.IDLE;
+    }
 }
---
+++ src/main/java/knc/simulator/model/ElevatorAction.java
@@ -0,0 +1,8 @@
+package knc.simulator.model;
+
+public enum ElevatorAction {
+    IDLE,
+    HOLD,
+    GOING_UP,
+    GOING_DOWN;
+}
