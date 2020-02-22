cfb58dc93d208b70c026c7ecf06caa2b65782861
Robin Larsson <robin_rtl@hotmail.com>
2020-02-22 04:37:29 +0100

    Tweaked layout

--- src/main/java/knc/simulator/controller/StartController.java
+++ src/main/java/knc/simulator/controller/StartController.java
@@ -12,7 +12,7 @@ public class StartController {
     private Button createButton;
 
     public void initialize() {
-        var availableFloors = new ImmutableObservableList<>(2, 3, 4, 5, 6);
+        var availableFloors = new ImmutableObservableList<>(2, 3, 4, 5, 6, 7, 8);
         storeysChoice.setItems(availableFloors);
         storeysChoice.setValue(availableFloors.get(0));
     }
